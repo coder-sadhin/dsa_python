627c5b5e34779e50aac0e8046e00259353dd1d75
akkas_dev <codersadhin@gmail.com>
2024-07-14 00:15:31 +0800

    stack push item

--- stack_using_linkedlist/stack_using_LL.py
+++ stack_using_linkedlist/stack_using_LL.py
@@ -7,3 +7,10 @@ class Node:
 class Stack:
     def __init__(self):
         self.top = None
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    
