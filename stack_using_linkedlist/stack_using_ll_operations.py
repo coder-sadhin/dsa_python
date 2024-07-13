bf52271bec752eab8f7e1fbe46866da74c377841
akkas_dev <codersadhin@gmail.com>
2024-07-14 00:41:13 +0800

    text editor on string using stack

--- stack_using_linkedlist/stack_using_ll_operations.py
+++ stack_using_linkedlist/stack_using_ll_operations.py
@@ -1,5 +1,6 @@
 from stack_using_LL import Stack
 
+# this operation are using for revarse an string using stack 
 def reverse_string(string):
     s = Stack()
     for i in string:
@@ -8,5 +9,28 @@ def reverse_string(string):
     while (not s.is_empty()):
         res = res + s.pop()
     print(res)
-    
-reverse_string("chokka")
+
+# reverse_string("chokka")
+
+# this operation are using for set string delete and undo and redo
+def text_editor(text,pattern):
+    u = Stack()
+    r = Stack()
+
+    for i in text:
+        u.push(i)
+
+    for i in pattern:
+        if i == 'u':
+            data = u.pop()
+            r.push(data)
+        else:
+            data = r.pop()
+            u.push(data)
+
+    res = ""
+    while(not u.is_empty()):
+        res = u.pop() + res
+    print(res) 
+
+# text_editor("akkas","uurruuuur")
