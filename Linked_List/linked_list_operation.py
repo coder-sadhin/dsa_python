3980aafed07b92fc8b2de85ffdf4792ff4a3e42e
akkas_dev <codersadhin@gmail.com>
2024-07-13 22:02:36 +0800

    insert on head

--- Linked_List/linked_list_operation.py
+++ Linked_List/linked_list_operation.py
@@ -4,8 +4,26 @@ class Node:
     self.next = None
 
 class LinkedList:
-  def __init__(self):
-    # Empty Linked List
-    self.head = None
-    # no of nodes in the LL
-    self.n = 0
+    # this is linked list constractor
+    def __init__(self):
+        # Empty Linked List
+        self.head = None
+        # no of nodes in the LL
+        self.n = 0
+
+    # this is for showing len of the list
+    def __len__(self):
+        return self.n
+    
+    # this is for insert value on head 
+    def insert_head(self,value):
+        # new node
+        new_node = Node(value)
+        # create connection
+        new_node.next = self.head
+        # reassign head
+        self.head = new_node
+        # increment n
+        self.n = self.n + 1
+
+
