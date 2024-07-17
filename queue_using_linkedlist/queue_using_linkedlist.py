cb99b571e5c7159b824db99d8f49f542ac512f4a
akkas_dev <codersadhin@gmail.com>
2024-07-17 22:11:06 +0800

    queue push data or enqueue function done

--- queue_using_linkedlist/queue_using_linkedlist.py
+++ queue_using_linkedlist/queue_using_linkedlist.py
@@ -4,8 +4,18 @@ class Node:
     self.next = None
 
 class Queue:
-  def __init__(self):
-    self.front = None
-    self.rear = None
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.front == None:
+            self.front = new_node
+            self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
 
 q = Queue()
