c0cacc58721227f12f31394b2696a0780ce32ce5
AkkasDev <codersadhin@gmail.com>
2024-07-21 22:12:30 +0800

    change some code

--- stack_using_linkedlist/stack_using_ll_operations.py
+++ stack_using_linkedlist/stack_using_ll_operations.py
@@ -53,6 +53,22 @@ def brackets(expr):
         print("Balanced")
     else:
         print("Imbalanced")
+# def brackets(expr):
+#     s = Stack()
+#     for i in expr:
+#         if i == '(':
+#             s.push(i)
+#         elif i == ')':
+#             if s.peek() == '(':
+#                 s.pop()
+#             else:
+#                 print("Imbalanced")
+#                 return 
+
+#     if (s.is_empty()):
+#         print("Balanced")
+#     else:
+#         print("Imbalanced")
 
 # brackets("(a+b)")
 
