80c5f32506637377d296bf08f379b342999c17be
AkkasDev <codersadhin@gmail.com>
2024-07-21 22:12:12 +0800

    change

--- stack_using_linkedlist/stack_using_ll_operations.py
+++ stack_using_linkedlist/stack_using_ll_operations.py
@@ -28,19 +28,6 @@ def text_editor(text,pattern):
         else:
             data = r.pop()
             u.push(data)
-    # u = Stack()
-    # r = Stack()
-
-    # for i in text:
-    #     u.push(i)
-
-    # for i in pattern:
-    #     if i == 'u':
-    #         data = u.pop()
-    #         r.push(data)
-    #     else:
-    #         data = r.pop()
-    #         u.push(data)
 
     res = ""
     while(not u.is_empty()):
